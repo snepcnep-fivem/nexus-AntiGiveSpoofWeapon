debc260b928f6f2bcea9f2a7ec477806cb9e68ae
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-01-04 20:26:21 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
 
+author 'SnepCnep'
+description 'Anti Give Spoof Weapon (Nexus/Phaze)'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
