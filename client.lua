1177e19a0c559abcb5672a6be5032cfb2d23fdfe
SnepCnep <93768063+SnepCnep@users.noreply.github.com>
2025-01-10 09:08:58 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,6 +3,8 @@ local _IsPedArmed = IsPedArmed
 local _GetCurrentPedWeapon = GetCurrentPedWeapon
 
 CreateThread(function()
+    local spawnWarns = 0 
+
     while true do
         Wait(1000)
         local PlayerPed = _PlayerPedId()
@@ -12,11 +14,13 @@ CreateThread(function()
         local isArmed3 = _IsPedArmed(PlayerPed, 4)
 
         if not isArmed1 and not isArmed2 and not isArmed3 then
+            spawnWarns = 0
             goto skipCheck
         end
 
         local weaponHoldStatus  = _GetCurrentPedWeapon(PlayerPed)
         if weaponHoldStatus then
+            spawnWarns = 0
             goto skipCheck
         end
 
@@ -27,7 +31,11 @@ CreateThread(function()
         end -- IDK if required but yhe......
 
         SetCurrentPedWeapon(PlayerPed, `WEAPON_UNARMED`, true)
-        TriggerServerEvent("sc-antiGiveWeapon:banPlayer", "Try to spawn a weapon (PHAZE) [SnepCnep-AntiGiveWeapon]")
+
+        if spawnWarns > 2 then
+            TriggerServerEvent("sc-antiGiveWeapon:banPlayer", "Try to spawn a weapon (PHAZE) [SnepCnep-AntiGiveWeapon]")
+        end
+        spawnWarns = spawnWarns + 1
 
         ::skipCheck::
     end
